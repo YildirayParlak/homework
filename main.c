a1f7b254f650355ae4041eb5a02a03ea60dfb108
YıldırayParlak <127852472+YildirayParlak@users.noreply.github.com>
2023-03-15 15:21:39 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,76 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct ogrenci1
-{
-	char isim[15];
-	char soyad[15];
-	int numara;
-	char adres[40];
-};
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct ogrenci1 bir={"Halil","Dikoglu",9,"Aydin kusadasi davutlar mah."};
-
-struct ogrenci2
-{
-	char isim[15];
-	char soyad[15];
-	int numara;
-	char adres[30];
-};
-
-struct ogrenci2 iki={"Emirhan","Kaytakli",49,"Tekirdag corlu ilcesi"};
-
-struct ogrenci3
-{
-	char isim[15];
-	char soyad[15];
-	int numara;
-	char adres[40];
-};
-
-struct ogrenci3 uc={"Yildiray","Parlak",16,"Gaziantep sehitkamil zeytinli mah"};
-
-struct ogrenci4
-{
-	char isim[15];
-	char soyad[15];
-	int numara;
-	char adres[30];
-};
-
-struct ogrenci4 dort={"Deniz","Aykut",25,"Malatya dogansehir ilcesi"};
-
-struct ogrenci5
-{
-	char isim[15];
-	char soyad[15];
-	int numara;
-	char adres[30];
-};
-
-struct ogrenci5 bes={"Cemal","Gungor",42,"Konya aksehir ilcesi"};
-
-int main ()
-{
-	printf("1. Ogrenci bilgileri\nIsim:%s\nSoyad:%s\nNumara:%d\nAdres:%s\n",bir.isim,bir.soyad,bir.numara,bir.adres);
+int main() {
+	int elemanSayisi;
+	printf("lutfen olusacak dizinin kac elemanlý oldugunu giriniz: ");
+	scanf("%d",&elemanSayisi);
+	int sayiDizisi[elemanSayisi];
+	srand(time(NULL));
+	int enKucuk,i;
 	
-	printf("*****************************\n");
 	
-	printf("2. Ogrenci bilgileri\nIsim:%s\nSoyad:%s\nNumara:%d\nAdres:%s\n",iki.isim,iki.soyad,iki.numara,iki.adres);
+	for(i=0;i<elemanSayisi;i++){
+		sayiDizisi[i]=rand()%100;
+		printf("%d",sayiDizisi[i]);
+		
+	}
 	
-	printf("*****************************\n");
+	enKucuk=sayiDizisi[0];
+	printf("\n");
 	
-	printf("3. Ogrenci bilgileri\nIsim:%s\nSoyad:%s\nNumara:%d\nAdres:%s\n",uc.isim,uc.soyad,uc.numara,uc.adres);
+	for(i=0;i<elemanSayisi;i++){
+		if(sayiDizisi[i]<enKucuk){
+			enKucuk=sayiDizisi[i];
+		}
+	}	
+	printf("dizinin en kucuk elemani: %d",enKucuk);	
 	
-	printf("*****************************\n");
 	
-	printf("4. Ogrenci bilgileri\nIsim:%s\nSoyad:%s\nNumara:%d\nAdres:%s\n",dort.isim,dort.soyad,dort.numara,dort.adres);
-	
-	printf("*****************************\n");
-	
-	printf("5. Ogrenci bilgileri\nIsim:%s\nSoyad:%s\nNumara:%d\nAdres:%s\n",bes.isim,bes.soyad,bes.numara,bes.adres);
 	
 	return 0;
 }
-	
-
